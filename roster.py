d0643ac233256ab92392e3e266084dc05a86761d
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-12-09 19:30:10 -0600

    Made point_totals function more dynamic
    Now you don't have to guess how long the longest competitor name is OR how many matches you're gonna have to play.

--- roster.py
+++ roster.py
@@ -3,6 +3,7 @@ Handles generating the competitors, leagues and holds the point_totals function.
 """
 
 import competitor_class as cc
+from math import ceil
 
 elIdolo = cc.create_competitor(cc.competitorDictionary["Andrade el Idolo"])
 danielson = cc.create_competitor(cc.competitorDictionary["Bryan Danielson"])
@@ -39,12 +40,14 @@ def point_totals(*leagues):
 Automatically formats the league participants into neat and tidy stat blocks.\n
 Mathematically-eliminated competitors are marked with strikethrough text.
     """
+    longestName = lambda leagueRoster: max([len(entrant.name) for entrant in leagueRoster])
+    lossLimit = lambda leagueMembers: ceil((len(leagueMembers) - 1) / 2)
     for league in leagues:
         print(f"{league.name.title()} League:")
         for guy in league.roster:
-            while len(guy.name) < 24:
+            while len(guy.name) < longestName(league.roster):
                 guy.name += " "
-            if guy.losses >= 3:
+            if guy.losses >= lossLimit(league.roster):
                 print("\u0336" + "\u0336".join(f"{guy.name}: {str(guy.points).zfill(2)} points ({guy.wins}-{guy.losses}-{guy.draws})"))
             else:
                 print(f"{guy.name}: {str(guy.points).zfill(2)} points ({guy.wins}-{guy.losses}-{guy.draws})", f"TBPs: {guy.TBP}", sep = "|")
